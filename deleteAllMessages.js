4837a92521cf2f1aba82afd90bf861c5d6a9ae18
daniel <danielvolosov@outlook.com>
2021-07-17 17:41:44 -0500

    script

--- deleteAllMessages.js
+++ deleteAllMessages.js
@@ -2,17 +2,22 @@ function getElementByXpath(path) {
   return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 }
 
-var ellipses = getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/button");
+var messagesExist = document.getElementsByClassName("msg-s-event-listitem__message-bubble msg-s-event-listitem__message-bubble--msg-fwd-enabled");
 
-ellipses.click();
+while (messagesExist) {
+    var ellipses = getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/button");
+    ellipses.click();
 
-var deleteMessage = getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/div/div/div/ul/div[2]");
-
-deleteMessage.click();
-
-var confirmDelete = getElementByXpath("/html/body/div[3]/div/div/div[3]/div/button[2]");
-
-confirmDelete.click();
-
-
+    var deleteMessage = getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/div/div/div/ul/div[2]");
+    
+    setTimeout(function() {
+        deleteMessage.click()
+    }, 5000);
+    
+    var confirmDelete = getElementByXpath("/html/body/div[3]/div/div/div[3]/div/button[2]");
+    
+    setTimeout(function() {
+        confirmDelete.click();
+    }, 5000);
+}
 
