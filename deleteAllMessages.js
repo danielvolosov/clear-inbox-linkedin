a5588899ecd50d597858fa5553b7c4e202326b99
daniel <danielvolosov@outlook.com>
2021-07-17 18:24:57 -0500

    Complete script

--- deleteAllMessages.js
+++ deleteAllMessages.js
@@ -5,19 +5,14 @@ function getElementByXpath(path) {
 var messagesExist = document.getElementsByClassName("msg-s-event-listitem__message-bubble msg-s-event-listitem__message-bubble--msg-fwd-enabled");
 
 while (messagesExist) {
-    var ellipses = getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/button");
-    ellipses.click();
+    
+    // Open dropdown 
+    getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/button").click();
 
-    var deleteMessage = getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/div/div/div/ul/div[2]");
+    // Select delete dropdown item
+    getElementByXpath("/html/body/div[5]/div[3]/div[2]/div/div/main/div/section[2]/div[1]/div[1]/div/div[1]/div/div/div/ul/div[2]").click();
     
-    setTimeout(function() {
-        deleteMessage.click()
-    }, 5000);
-    
-    var confirmDelete = getElementByXpath("/html/body/div[3]/div/div/div[3]/div/button[2]");
-    
-    setTimeout(function() {
-        confirmDelete.click();
-    }, 5000);
+    // Confirm message deletion
+    getElementByXpath("/html/body/div[3]/div/div/div[3]/div/button[2]").click();
 }
 
